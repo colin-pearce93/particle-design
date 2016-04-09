c3e8c9e5ac725456c8efa5bee7a4454707ccd023
Colin Campbell <colinpc93@gmail.com>
2016-04-09 16:49:28 -0400

    ts

--- app.js
+++ app.js
@@ -17,20 +17,20 @@ app.set('view engine', 'ejs');
 
 
 
-if (!(app.get('env') === 'development')) {
+// if (!(app.get('env') === 'development')) {
 
-  app.enable('trust proxy');
+//   app.enable('trust proxy');
 
-  app.use (function (req, res, next) {
-      if (req.secure) {
-              // request was via https, so do no special handling
-              next();
-      } else {
-              // request was via http, so redirect to https
-              res.redirect('https://' + req.headers.host + req.url);
-      }
-  });
-}
+//   app.use (function (req, res, next) {
+//       if (req.secure) {
+//               // request was via https, so do no special handling
+//               next();
+//       } else {
+//               // request was via http, so redirect to https
+//               res.redirect('https://' + req.headers.host + req.url);
+//       }
+//   });
+// }
 
 
 // uncomment after placing your favicon in /public
