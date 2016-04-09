99ffcfefaf9dbe618c4b7881839adf21c8b0fc37
Colin Campbell <colinpc93@gmail.com>
2016-04-09 19:48:13 -0400

    ts

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.enable('trust proxy');
+ 
 
 // if (!(app.get('env') === 'development')) {
 
